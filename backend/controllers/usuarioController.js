a40c9c6ffd5732f31a9585410c98ec65d2338f7b
Diego <diegocuchodiaz151617@gmail.com>
2026-05-17 22:24:19 -0500

    cambios24

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -310,7 +310,7 @@ const comprarLibros = async (req, res) => {
         }
 
         // 3. Intentamos enviar el correo
-        const remitente = process.env.RESEND_FROM_EMAIL || "onboarding@resend.dev";
+        const remitente = process.env.RESEND_FROM_EMAIL || "diegocuchodiaz151617@gmail.com";
 
         try {
             const data = await resend.emails.send({
