ecaf8ac269a6bc9af9d5263880b8e55385fa5734
Diego <diegocuchodiaz151617@gmail.com>
2026-05-17 19:54:51 -0500

    cambios14

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,7 @@ const app = express();
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI, {});
         console.log("✅ Conexión exitosa a MongoDB Atlas");
     } catch (error) {
         console.error("❌ Error al conectar a MongoDB:", error.message);
